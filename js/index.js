a0d29179427d81d30a976ae1d884e8a3281386e8
Akshayakt <akshaya@qburst.com>
2016-02-01 14:24:54 +0530

    added right drawer

--- js/index.js
+++ js/index.js
@@ -4,3 +4,10 @@ document.getElementById('hamburger').addEventListener('click', drawer, false);
 function drawer() {
     document.getElementById('drawer').classList.toggle('drawer_left');
 }
+
+document.getElementById('new_articles').addEventListener('click', drawerRight, false);
+
+function drawerRight() {
+   document.getElementById('right_drawer').classList.toggle('drawer_right');
+// alert("asdsa");
+}
